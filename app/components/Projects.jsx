965eee93ee0c31e675b11ae9cf74b8aaf806de28
viviannqin <vivianq0420@gmail.com>
2024-12-30 14:27:28 -0700

    added filters to projects

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import ProjectCard from './ProjectCard'
+import ProjectTag from './ProjectTag'
 
 const projectsData = [
     {
@@ -21,6 +23,16 @@ const projectsData = [
   ];
 
 const Projects = () => {
+  const [tag, setTag] = useState("All");
+
+  const handleTagChange = (newTag) => {
+    setTag(newTag);
+  };
+
+  const filteredProjects = projectsData.filter((project) =>
+    project.tag.includes(tag)
+  );
+
   return (
     <>
         <h2 className="text-white text-3xl sm:text-4xl lg:text-5xl font-extrabold mb-8 text-center">
@@ -28,19 +40,25 @@ const Projects = () => {
             My Projects
           </span>
         </h2>
-        <div className="text-black flex flex-row justify-center items-center gap-2 py-6">
-            <button className="rounded-full border-2 border-black px-6 py-2 text-xl cursor-pointer">
-                All
-            </button>
-            <button className="rounded-full border-2 border-slate-300 hover:border-black px-6 py-2 text-xl cursor-pointer">
-                Hackathons
-            </button>
-            <button className="rounded-full border-2 border-slate-300 hover:border-black px-6 py-2 text-xl cursor-pointer">
-                Personal
-            </button>
+        <div className="flex flex-row justify-center items-center gap-2 py-6">
+            <ProjectTag
+                onClick={handleTagChange}
+                name="All"
+                isSelected={tag === "All"}
+            />
+            <ProjectTag
+                onClick={handleTagChange}
+                name="Hackathon"
+                isSelected={tag === "Hackathon"}
+            />
+            <ProjectTag
+                onClick={handleTagChange}
+                name="Personal"
+                isSelected={tag === "Personal"}
+            />
         </div>
         <div className="grid md:grid-cols-3 gap-8 md:gap-12">
-            {projectsData.map((project) => (
+            {filteredProjects.map((project) => (
                 <ProjectCard 
                     key={project.id} 
                     title={project.title} 
